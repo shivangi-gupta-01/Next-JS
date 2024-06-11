7f4228363c05589ad7bf68b505cf31a89e2605d6
shivangi-gupta-01 <110471751+shivangi-gupta-01@users.noreply.github.com>
2024-06-11 11:30:06 +0530

    Image

--- app/page.js
+++ app/page.js
@@ -1,25 +1,36 @@
 // import Image from "next/image";
-'use client'
-
+// 'use client'
+ 
 import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Photo from "../public/vercel.svg"
 
 export default function Home() {
-  const router = useRouter()
-  const [name, setName] = useState('shivangi')
-  const navigate =(name)=>{
-    router.push(name)
-  }
-  const handle = ()=>{
-    setName('gupta')
-  }
+  // const router = useRouter()
+  // const [name, setName] = useState('shivangi')
+  // const navigate =(name)=>{
+  //   router.push(name)
+  // }
+  // const handle = ()=>{
+  //   setName('gupta')
+  // }
+  console.log(Photo);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Events {name}</h1>
+    <main>
+      {/* <h1>Events {name}</h1>
       <button onClick={()=>handle()}>Click</button>
       <Link href="/login">go to login</Link>
-      <button onClick={()=>{navigate('/login')}}>login page</button>
+      <button onClick={()=>{navigate('/login')}}>login page</button> */}
+
+
+
+
+
+      {/* Image optimization */}
+      <h1>Image optimization</h1>
+      <Image src={Photo} />
     </main>
   );
 }
